e1a863563a20fbd8ddc8ce61d5e6265b81b433e6
Oleg Shapoval <olegkazhan@gmail.com>
2025-03-30 16:05:36 +0300

    client.ts => export like supabaseClient

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,8 +2,9 @@
 import { createClient as createBrowserClient } from '@supabase/supabase-js'
 
 
-export const createClient = () =>
+const createClient = () =>
     createBrowserClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     );
+export const supabaseClient = createClient();
