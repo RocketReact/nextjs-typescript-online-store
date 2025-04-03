3469bd609c2673b69e767b6fd0e17fd0e2b6421a
Oleg Shapoval <olegkazhan@gmail.com>
2025-04-03 12:14:06 +0300

    store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
+import filterSliceReducer from './slices/filterSlice'
+
+export const store = configureStore({
+    reducer: {
+        filters: filterSliceReducer
+    },
+
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
